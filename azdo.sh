1b2b00051ec788ed0473721ebcf272f443c15c54
Bax Picker <baxpicker@gmail.com>
2025-09-09 14:37:59 +0200

    Add AzureDevOps and GitHub helpers

---
+++ azdo.sh
@@ -0,0 +1,128 @@
+# include guard
+if [[ -n "$_AZDO_SH_INCLUDED" ]]; then
+  return
+fi
+_AZDO_SH_INCLUDED=1
+
+# absolute path to root folder
+if [[ "${FOLDER_bash}" == "" ]]; then
+    echo "FOLDER_bash not set"
+    exit 1
+fi
+
+# includes
+# ########
+
+source "${FOLDER_bash}/sanity.sh"
+source "${FOLDER_bash}/logging.sh"
+
+# sanity
+# ######
+
+ensure_command az
+az extension show --name azure-devops >/dev/null 2>&1 || \
+    { log_error "Azure DevOps extension for Azure CLI not installed. Please run 'az extension add --name azure-devops'"; }
+
+# functions
+# #########
+
+function azdo_set_pat() {
+
+    log_info "Set Azure DevOps PAT..."
+
+    # defaults
+    local pat=""
+
+    # Parse named arguments
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+            --pat)
+            pat="$2"
+            shift
+            shift
+            ;;
+        esac
+    done
+
+    { \
+        [[ ${pat} != "" ]] \
+    } || { log_error "Function argument missing"; }
+
+    export AZURE_DEVOPS_EXT_PAT="${pat}"
+
+    az devops project list >/dev/null 2>&1 || { log_error "Given PAT is not valid"; }
+
+    log_info "Set Azure DevOps PAT finished successfully"
+}
+
+# Set or create an Azure DevOps pipeline variable
+function azdo_pipeline_set_var() {
+
+    log_info "Setting AZDO var '${var_name}'"
+
+    # defaults
+    local var_name=""
+    local var_value=""
+    local var_pipeline_id=""
+    local var_secret="true"
+    local var_override="true"
+
+    # Parse named arguments
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+            --name)
+            var_name="$2"
+            shift
+            shift
+            ;;
+            --value)
+            var_value="$2"
+            shift
+            shift
+            ;;
+            --pipeline-id)
+            var_pipeline_id="$2"
+            shift
+            shift
+            ;;
+            --secret)
+            var_secret="$2"
+            shift
+            shift
+            ;;
+            --override)
+            var_override="$2"
+            shift
+            shift
+            ;;
+        esac
+    done
+
+    { \
+        [[ ${var_name} != "" ]] && \
+        [[ ${var_value} != "" ]] && \
+        [[ ${var_pipeline_id} != "" ]] && \
+        [[ ${var_secret} != "" ]] && \
+        [[ ${var_override} != "" ]] \
+    } || { log_error "Function argument missing"; }
+
+    log_info "Setting AZDO var '${var_name}'"
+
+    az pipelines variable update \
+        --pipeline-id "${var_pipeline_id}" \
+        --name "${var_name}" \
+        --value "${var_value}" \
+        --allow-override "${var_override}" \
+        --secret "${var_secret}" >/dev/null 2>&1 \
+    || \
+    az pipelines variable create \
+        --pipeline-id "${var_pipeline_id}" \
+        --name "${var_name}" \
+        --value "${var_value}" \
+        --allow-override "${var_override}" \
+        --secret "${var_secret}" >/dev/null 2>&1
+
+    [[ $? -eq 0 ]] || { log_error "Setting AZDO var '${var_name}' failed"; }
+}
---
+++ gh.sh
@@ -0,0 +1,121 @@
+# include guard
+if [[ -n "$_GH_SH_INCLUDED" ]]; then
+  return
+fi
+_GH_SH_INCLUDED=1
+
+# absolute path to root folder
+if [[ "${FOLDER_bash}" == "" ]]; then
+    echo "FOLDER_bash not set"
+    exit 1
+fi
+
+# includes
+# ########
+
+source "${FOLDER_bash}/sanity.sh"
+source "${FOLDER_bash}/logging.sh"
+
+# sanity
+# ######
+
+ensure_command gh
+
+# functions
+# #########
+
+function gh_login() {
+
+    log_info "Logging in to Github..."
+
+    # defaults
+    local pat=""
+    local hostname="github.com"
+
+    # Parse named arguments
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+            --pat)
+            pat="$2"
+            shift
+            shift
+            ;;
+            --hostname)
+            hostname="$2"
+            shift
+            shift
+            ;;            
+        esac
+    done
+
+    { \
+        [[ ${pat} != "" ]] && \
+        [[ ${hostname} != "" ]] \
+    } || { log_error "Function argument missing"; }
+
+    echo "${pat}" |run gh auth login --hostname "${hostname}" --with-token
+    run gh auth status
+
+    log_info "Logged in to Github successfully"
+}
+
+# Set or create an GitHub variable on a repo
+function gh_repo_set_var() {
+
+    log_info "Setting GH var '${var_name}'"
+
+    # defaults
+    local var_name=""
+    local var_value=""
+    local var_repo=""
+    local var_secret="true"
+
+    # Parse named arguments
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+            --name)
+            var_name="$2"
+            shift
+            shift
+            ;;
+            --value)
+            var_value="$2"
+            shift
+            shift
+            ;;
+            --repo)
+            var_repo="$2"
+            shift
+            shift
+            ;;
+            --secret)
+            var_secret="$2"
+            shift
+            shift
+            ;;
+        esac
+    done
+
+    { \
+        [[ ${var_name} != "" ]] && \
+        [[ ${var_value} != "" ]] && \
+        [[ ${var_repo} != "" ]] && \
+        [[ ${var_secret} != "" ]] \
+    } || { log_error "Function argument missing"; }
+
+    log_info "Setting GH var '${var_name}'"
+
+    local gh_cmd="gh secret set"
+    if [[ "${var_secret}" != "true" ]]; then
+        gh_cmd="gh variable set"
+    fi
+
+    ${gh_cmd} \
+        ${var_name} \
+        --repo "${var_repo}" \
+        --body "${var_value}" >/dev/null 2>&1
+
+    [[ $? -eq 0 ]] || { log_error "Setting GH var '${var_name}' failed"; }
+}
